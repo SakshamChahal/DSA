a4ceaa2fc9e25559ec145da3a4d4e943e2fdc6ff
SakshamChahal <148672102+SakshamChahal@users.noreply.github.com>
2025-07-02 10:43:31 +0530

    two-sum.java

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -2,7 +2,7 @@ import java.util.HashMap;
 
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>(); // stores value : index
+        HashMap<Integer, Integer> map = new HashMap<>(); 
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
             if (map.containsKey(complement)) {
@@ -10,6 +10,6 @@ public class Solution {
             }
             map.put(nums[i], i);
         }
-        return new int[] {}; // guaranteed to have one solution, so this won't execute
+        return new int[] {}; 
     }
 }
